1627da85f8ef1af322ad42962faa1725c8759fcc
RoseDiaz <diaz.sanchez.rose@gmail.com>
2022-06-23 14:04:48 -0500

    Se agregaron dos botones con imagenes y se agrego el scroll para desplazarnos sobre el texto de arriba hacia abajo

--- app/src/main/java/com/example/practica1/MainActivity.kt
+++ app/src/main/java/com/example/practica1/MainActivity.kt
@@ -5,9 +5,11 @@ package com.example.practica1
 import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 
 //enlazar elementos de la vista con el codigo
 import com.example.practica1.databinding.ActivityMainBinding
+import kotlin.system.exitProcess
 
 //La actividad principal (MainActivity) contiene la lógica de nuestra pantalla.
 class MainActivity : Activity() {
@@ -27,5 +29,13 @@ class MainActivity : Activity() {
         leyenda.text="Bienvenido a mi primer aplicación para SmartWatch"*/
         //binding.text.text="Bienvenido a mi primer aplicación para SmartWatch"
 
+        binding.btnOk.setOnClickListener { //accion al hacer clic en el botón
+            Toast.makeText(this,"Aceptar",Toast.LENGTH_SHORT).show() //Muestra un mensaje
+        }
+
+        binding.btnCancel.setOnClickListener { //accion al hacer clic en el botón
+            Toast.makeText(this,"Adiós",Toast.LENGTH_SHORT).show() //Muestra un mensaje
+            exitProcess(0)  //Cierra la aplicación
+        }
     }
 }
